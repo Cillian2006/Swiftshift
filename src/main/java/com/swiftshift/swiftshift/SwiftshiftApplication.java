1696ced91d09fe0b53620db47ae16653547b078b
Cillian2006 <cillian2055@gmail.com>
2025-06-26 16:25:10 +0100

    Add Spring Boot setup and Greeting API

---
+++ src/main/java/com/swiftshift/swiftshift/SwiftshiftApplication.java
@@ -0,0 +1,13 @@
+package com.swiftshift.swiftshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftshift/swiftshift/SwiftshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftshift.swiftshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
